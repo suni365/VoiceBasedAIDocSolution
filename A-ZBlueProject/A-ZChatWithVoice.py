e1bf899d548f9c8e682098e3bf366a4feb0c99d1
suni365 <55737330+suni365@users.noreply.github.com>
2025-04-21 17:27:12 +0530

    Update A-ZChatWithVoice.py

--- A-ZBlueProject/A-ZChatWithVoice.py
+++ A-ZBlueProject/A-ZChatWithVoice.py
@@ -209,7 +209,16 @@ else:
                 async_processing=True,
                 rtc_configuration={
                     "iceServers": [
-                        {"urls": ["stun:stun.l.google.com:19302"]}  # Free STUN
+                        {"urls": ["stun:stun.l.google.com:19302"]}, # Free STUN
+                        {
+                            "urls": [
+                                "turn:openrelay.metered.ca:80",
+                                "turn:openrelay.metered.ca:443",
+                                "turn:openrelay.metered.ca:443?transport=tcp"
+                            ],
+                            "username": "openrelayproject",
+                            "credential": "openrelayproject"
+                        }
                     ]
                 }
             )
