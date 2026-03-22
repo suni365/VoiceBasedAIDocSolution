3e8e9d04b1ef5b84fd7e0a74e395a49109854831
suni365 <55737330+suni365@users.noreply.github.com>
2026-03-22 23:48:17 +0530

    Update aws_log_analysis.py

--- aws_log_analysis.py
+++ aws_log_analysis.py
@@ -12,7 +12,8 @@ st.set_page_config(
 
 # ---------------- API CONFIG ----------------
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+# model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # ---------------- COMMON AI FUNCTION ----------------
 def analyze_with_ai(prompt, image=None):
